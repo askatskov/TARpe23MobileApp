75db58b1df7798fe28ddf29273f1da0760978e2c
Opilane TTHK <opilane@tthk.ee>
2025-02-14 14:12:17 +0200

    planetsservice, lisatud said kõik planeedid ning pooleli jäi planetsdetailspage

--- MauiPlanets/MauiPlanets/Services/PlanetsService.cs
+++ MauiPlanets/MauiPlanets/Services/PlanetsService.cs
@@ -9,10 +9,11 @@ namespace Services
     {
         private static List<Planet> planets = new()
         {
-            new()
+new()
             {
                 Name = "Mercury",
-                Subtitle = "mercury.png",
+                Subtitle = "The smallest planet",
+                HeroImage = "mercury.png",
                 Description = "Mercury is the first planet from the Sun and the smallest in the Solar System. In English, it is named after the ancient Roman god Mercurius (Mercury), god of commerce and communication, and the messenger of the gods. Mercury is classified as a terrestrial planet, with roughly the same surface gravity as Mars. The surface of Mercury is heavily cratered, as a result of countless impact events that have accumulated over billions of years.",
                 AccentColorStart = Color.FromArgb("#353535"),
                 AccentColorEnd = Color.FromArgb("#8d9098"),
@@ -22,8 +23,116 @@ namespace Services
                     "https://solarsystem.nasa.gov/system/feature_items/images/73_carousel_mercury_2.jpg",
                     "https://solarsystem.nasa.gov/system/feature_items/images/75_mercury_carousel_1.jpg"
                 }
-            }
+            },
+            new()
+            {
+                Name = "Venus",
+                Subtitle = "The pressure cooker",
+                HeroImage = "venus.png",
+                Description = "Venus is the second planet from the Sun. It is a terrestrial planet and is the closest in mass and size to its orbital neighbour Earth. Venus has by far the densest atmosphere of the terrestrial planets, composed mostly of carbon dioxide with a thick, global sulfuric acid cloud cover. At the surface it has a mean temperature of 737 K (464 °C; 867 °F) and a pressure 92 times that of Earth's at sea level.",
+                AccentColorStart = Color.FromArgb("#a6393b"),
+                AccentColorEnd = Color.FromArgb("#d17f21"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/143_Venus-800x600.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/147_Hubble_Venus_Clouds-800x600.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/146_venus_carousel_3.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/144_PIA00485_Venus_Caldera-800.jpg"
+                }
+            },
+            new()
+            {
+                Name = "Earth",
+                Subtitle = "The cradle of life",
+                HeroImage = "earth.png",
+                Description = "The Earth is the only planet known where life exists. Almost 1.5 million species of animals and plants have been discovered so far, and many more have yet to be found. While other planets may have small amounts of ice or steam, the Earth is 2/3 water. Earth has perfect conditions for a breathable atmosphere. ",
+                AccentColorStart = Color.FromArgb("#0e3d68"),
+                AccentColorEnd = Color.FromArgb("#2e97c7"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/68_epicearthmoonstill_800.png",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/69_earth_carousel_3.jpg",
+                    "https://images.newscientist.com/wp-content/uploads/2019/09/09162708/iss048-e-2035_lrg.jpg?width=778"
+                }
+            },
+            new()
+            {
+                Name = "Mars",
+                Subtitle = "The red beauty",
+                HeroImage = "mars.png",
+                Description = "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, after Mercury. Its red color is due to iron oxide, or rust, on its surface. Mars has two moons, Phobos and Deimos, and is a target for future human exploration. The planet's past suggests that it may have had conditions suitable for life.",
+                AccentColorStart = Color.FromArgb("#a23036"),
+                AccentColorEnd = Color.FromArgb("#eb3333"),
+                Images = new()
+                {
+                    "https://researchfdi.com/wp-content/uploads/2021/10/8944_1-PIA24546-1280.jpeg",
+                    "https://www.worldatlas.com/r/w1300-q80/upload/bb/c3/32/shutterstock-1041249343.jpg",
+                    "https://www.openaccessgovernment.org/wp-content/uploads/2021/04/dreamstime_xxl_121672573-scaled.jpg"
+                }
+            },
+            new()
+            {
+                Name = "Jupiter",
+                Subtitle = "The gas giant",
+                HeroImage = "jupiter.png",
+                Description = "Jupiter is the fifth and largest planet from the Sun, with a mass that is more than twice that of all the other planets in the Solar System combined. It is a gas giant with a massive atmosphere primarily composed of hydrogen and helium. Jupiter has 79 known moons and a famous storm known as the Great Red Spot.",
+                AccentColorStart = Color.FromArgb("#9d4a40"),
+                AccentColorEnd = Color.FromArgb("#cd8026"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/11_Full_Jupiter-800.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/10_Jupiter_Io_Juno-800.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/13_PIA24237_Jupiter_Cyclones-800.jpg"
+                }
+            },
+            new()
+            {
+                Name = "Saturn",
+                Subtitle = "The ring planet",
+                HeroImage = "saturn.png",
+                Description = "Saturn is the sixth planet from the Sun and is best known for its extensive ring system, which is the most developed of any planet in the Solar System. Saturn is a gas giant and has at least 82 moons. It is the second-largest planet in the Solar System and has been explored by the Cassini spacecraft.",
+                AccentColorStart = Color.FromArgb("#996237"),
+                AccentColorEnd = Color.FromArgb("#c6502f"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/151_saturn_carousel_1.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/152_saturn_carousel_2.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/155_saturn_carousel_5.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/153_saturn_carousel_3.jpg"
+                }
+            },
+            new()
+            {
+                Name = "Uranus",
+                Subtitle = "The Herschel planet",
+                HeroImage = "uranus.png",
+                Description = "Uranus is the seventh planet from the Sun and is unique due to its extreme tilt, which makes it appear to roll on its side as it orbits the Sun. It is a gas giant with a cold atmosphere composed mainly of hydrogen, helium, and methane. Uranus has 27 known moons and was the first planet discovered with a telescope.",
+                AccentColorStart = Color.FromArgb("#9d4a40"),
+                AccentColorEnd = Color.FromArgb("#996237"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/88_carousel_uranus.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/89_uranus_carousel_1.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/88_carousel_uranus.jpg"
+                }
+            },
+            new()
+            {
+                Name = "Neptune",
+                Subtitle = "The god of the sea",
+                HeroImage = "neptune.png",
+                Description = "Neptune is the eighth and farthest planet from the Sun in the Solar System. It is a gas giant with a deep blue color due to methane in its atmosphere. Neptune has 14 known moons, and its largest moon, Triton, is unique because it orbits in the opposite direction of the planet's rotation.",
+                AccentColorStart = Color.FromArgb("#0c293d"),
+                AccentColorEnd = Color.FromArgb("#26abe0"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/82_carousel_neptune_1.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/86_carousel_neptune_5.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/84_neptune_carousel_2.jpg"
+                }
+            },
         };
+
         public static List<Planet> GetFeaturedPlanets()
         {
             var random = new Random();
